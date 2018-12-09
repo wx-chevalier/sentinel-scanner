37a2bc8fda5e2c1e5b18aa98ba1d4ffde0397766
wxyyxc1992 <384924552@qq.com>
2018-12-09 21:03:25 +0800

    feat: add logger

--- crawlers/cendertron/src/crawler/supervisor/logger.ts
+++ crawlers/cendertron/src/crawler/supervisor/logger.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import * as winston from 'winston';
 
-const rootDir = path.join('~', 'logs', 'cendertron');
+const rootDir = path.join('/root', 'logs', 'cendertron');
 
 export const logger = winston.createLogger({
   level: 'info',
