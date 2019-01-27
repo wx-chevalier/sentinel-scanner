f71afd8198600cd7144b49e8fc8fd1a1fcfdc98e
wxyyxc1992 <384924552@qq.com>
2019-01-27 19:50:31 +0800

    feat: rm cendertron

--- crawlers/cendertron/src/crawler/constants.ts
+++
@@ -1,2 +0,0 @@
-// 定义内部使用的方法常量
-export const _SPIDER_addRequest = Symbol('_SPIDER_addRequest');
--- crawlers/cendertron/src/shared/__test__/model.test.ts
+++
@@ -1,9 +0,0 @@
-import { hashUrl } from '../model';
-
-// console.log(hashUrl({ url: 'https://www.patreon.com/bePatron?c=784328' }));
-// console.log(
-//   hashUrl({ url: 'https://jsonplaceholder.typicode.com/posts/1/comments' })
-// );
-console.log(
-  hashUrl({ url: 'http://new.qq.com/omn/20181125/20181125A1EK6Q.html' })
-);
